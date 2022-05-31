ea2746207ab02d09af9acf00be51db1e698fb5b7
Joseph Bano <andres.jbo2003@gmail.com>
2022-05-31 10:34:27 -0500

    Agregar clase persona

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
+#include "persona.h"
 using namespace std;
 
 int main()
 {
     cout << "Primer proyecto de Joseph Bano en GIT y GitHub" << endl;
+    persona p;
+    p.edad=18;
+    p.nombre="Joseph";
+    cout << "Nombre: " << p.nombre << "\nEdad: " << p.edad;
     return 0;
 }
---
+++ persona.h
@@ -0,0 +1,13 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+#include "string"
+
+class persona
+{
+public:
+    std::string nombre;
+    int edad;
+    persona();
+};
+
+#endif // PERSONA_H
